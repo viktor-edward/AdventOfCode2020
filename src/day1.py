120efec4040a39868f9d5e4ea9bbd2423526ba9e
Viktor <viktor.edward@visigon.se>
2020-12-01 07:01:47 +0100

    AoC day 1 completed.

--- src/day1.py
+++ src/day1.py
@@ -10,33 +10,40 @@ def readFile(fileInput):
     return data
 
 
-def calculateFuelRequired(mass):
-    return math.floor(mass/3) - 2
+def findMultiNumber(data, numberToFind):
+    numberOfPoints = len(data)
+    for i in range(0,numberOfPoints):
+        for j in range(1,numberOfPoints):
+            if data[i]+data[j] == numberToFind:
+                return data[i], data[j]
+    print("Number not found")
+    return 0, 0
 
 
-def calculateFuelRequiredRecursive(mass):
-    fuelReqCurrent = calculateFuelRequired(mass)
-    if fuelReqCurrent > 0:
-        fuelReq = fuelReqCurrent + calculateFuelRequiredRecursive(fuelReqCurrent)
-        return fuelReq
-    else:
-        return 0
+def findMultiNumberThree(data, numberToFind):
+    numberOfPoints = len(data)
+    for i in range(0,numberOfPoints):
+        for j in range(1,numberOfPoints):
+            for k in range(1, numberOfPoints):
+                if data[i]+data[j]+data[k] == numberToFind:
+                    return data[i], data[j], data[k]
+    print("Number not found")
+    return 0, 0
 
 
 def main():
-    listOfMass = readFile("../resources/day1_input.txt")
+    data = readFile("../resources/day1_input.txt")
 
     print("Part one: ")
-    listOfFuelRequired = []
-    for mass in listOfMass:
-        listOfFuelRequired.append(calculateFuelRequired(mass))
-    print(sum(listOfFuelRequired))
+    numberToFind = 2020
+    n1, n2 = findMultiNumber(data, numberToFind)
+    print("Numbers were: " + str(n1) + " and " + str(n2))
+    print(str(n1*n2))
 
     print("Part two: ")
-    listOfFuelRequiredRec = []
-    for mass in listOfMass:
-        listOfFuelRequiredRec.append(calculateFuelRequiredRecursive(mass))
-    print(sum(listOfFuelRequiredRec))
+    n1, n2, n3 = findMultiNumberThree(data, numberToFind)
+    print("Numbers were: " + str(n1) + " and " + str(n2)+ " and " + str(n3))
+    print(str(n1*n2*n3))
 
 
 if __name__ == '__main__':
